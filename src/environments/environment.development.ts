9f73a732508da6d0e47540d1f2c4b32c03fd10d2
Aryan1982 <your-email@example.com>
2024-01-27 18:01:35 +0530

    envirionment.ts changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    url:'https://page-palette.onrender.com'
+    // url:'https://page-palette.onrender.com'
+    // url:'http://localhost:3000'
+    url:'https://palette.azurewebsites.net'
 };
